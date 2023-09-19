d7bb233f542b5a99aa2d1759c3399c495a5da9bc
Fibi Youssef <fibi057057@gmail.com>
2023-09-18 19:55:18 -0500

    converting jsx to tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,6 +10,7 @@ import {
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
+import React from "react";
 
 const { Header, Content, Footer, Sider } = Layout;
 
