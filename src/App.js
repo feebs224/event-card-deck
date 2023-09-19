00b988bdd45f2672e999ee3e4a88d22a510d2d85
Fibi Youssef <fibi057057@gmail.com>
2023-09-18 20:50:03 -0500

    fixing navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
+import { Layout } from "antd";
 import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
-}
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
 
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Layout
+        style={
+          collapsed
+            ? { marginLeft: "80px", transitionDuration: "0.2s" }
+            : { marginLeft: "250px", transitionDuration: "0.2s" }
+        }
+      >
+        <NavBar
+          collapsed={collapsed}
+          setCollapsed={setCollapsed}
+          colorBgContainer={"#f6f8fb"}
+        />
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
